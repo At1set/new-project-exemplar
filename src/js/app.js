d5a488bf69ce870ed495748366ca67c5b17eb487
At1set <iliya9874329@gmail.com>
2023-07-26 15:25:56 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import * as flsFunctions from "./modules/functions.js"
+
+flsFunctions.isWebp()
+
+//Меню-бургер
+//import "./modules/burger-menu.js"
+
+//Динамическое адаптирование
+//flsFunctions.DynamicAdapt()
+
+//Слайдер
+//import "./modules/swiper.js"
+
+//Включение спойлеров
+//flsFunctions.spollers()
+
+//Основной js файл
+import "./modules/script.js"
